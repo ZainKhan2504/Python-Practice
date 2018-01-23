bbc3fa2496e5611ab5634013fde6c760d260233f
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-01-23 15:32:09 +0500

    Used set function which can make a set of same name values

---
+++ 6# Dictionaries/SetFunction.py
@@ -0,0 +1,7 @@
+favorite_languages = {'jen':'python',
+                      'sarah':'C',
+                      'edward':'ruby',
+                      'phil':'python'}
+
+for language in set(favorite_languages.values()):
+    print(language)
