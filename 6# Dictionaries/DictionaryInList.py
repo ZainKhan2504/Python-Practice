c9875627a221dd54ec00d5fad47d5736ce9f9c3f
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-01-23 15:36:40 +0500

    Stored the values of dictionaries into list

---
+++ 6# Dictionaries/DictionaryInList.py
@@ -0,0 +1,7 @@
+alien1 = {'color':'red','point':'5'}
+alien2 = {'color':'green','point':'15'}
+alien3 = {'color':'blue','point':'25'}
+
+alien = [alien1,alien2,alien3]
+for alien in alien:
+    print(alien)
