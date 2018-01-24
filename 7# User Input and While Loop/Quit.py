31ed3e800182e7933673e7fd8bdcf8c3f2918f96
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-01-24 13:36:48 +0500

    Use quit statement to terminate the program

---
+++ 7# User Input and While Loop/Quit.py
@@ -0,0 +1,6 @@
+prompt = "\nTell me something, and I will repeat it back to you:\nEnter 'quit' to end the program: "
+message= ""
+while message != 'quit':
+    message=input(prompt)
+    if message != 'quit':
+        print(message)
