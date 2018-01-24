3b023a72218a600ee4b91c0b8d78cf07aec5d0ea
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-01-24 13:45:24 +0500

    Use flag statement to terminate the program

---
+++ 7# User Input and While Loop/Flag.py
@@ -0,0 +1,8 @@
+prompt = "\nTell me something, and I will repeat it back to you:\nEnter 'quit' to end the program: "
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
