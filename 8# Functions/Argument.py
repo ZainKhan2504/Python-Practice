98e827826b911d9ee678c2bed08bf5bf9eb82c5b
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-01-24 15:31:15 +0500

    Passing argument is function

---
+++ 8# Functions/Argument.py
@@ -0,0 +1,6 @@
+def username(firstname,lastname):
+    print("\nMy First name is: "+firstname)
+    print("My last name is: "+lastname)
+    print("My full name is: "+firstname+" "+lastname)
+username("Zain","Khan")
+username("John","Hasting")
