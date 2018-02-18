1672a69de5875b30b3030d9d4fbb423b718edc60
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-02-18 17:42:20 +0500

    Applying Statistics Functions

---
+++ 10# Files and Exceptions/StatisticsFunctions.py
@@ -0,0 +1,21 @@
+import statistics
+
+with open('E:\SSUET Course AI\Python-Practice\\10# Files and Exceptions\\Statistics Functions File\\100numbers.txt') as f:
+    lines = f.readlines()
+    list = []
+    for line in lines:
+        list.append(int(line))
+    print("The List is:\n"+str(list))
+    mean = statistics.mean(list)
+    print("The 'Mean' of these numbers are: "+str(mean))
+    median = statistics.median(list)
+    print("The 'Median' of these numbers are: "+str(median))
+    mode = statistics.mode(list)
+    print("The 'Mode' of these numbers are: "+str(mode))
+    variance = statistics.pvariance(list)
+    print("The 'Variance' of these numbers are: "+str(variance))
+    standardDeviation = statistics.pstdev(list)
+    print("The 'Standard Deviation' of these numbers are: " + str(standardDeviation))
+
+
+
